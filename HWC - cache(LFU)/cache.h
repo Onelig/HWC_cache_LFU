b14442f5fc563bca673eb9ed62a86990559c1a40
onelig ⠀ <onelig.dark@gmail.com>
2024-11-01 18:40:32 +0200

    realize emplace method

--- HWC - cache(LFU)/cache.h
+++ HWC - cache(LFU)/cache.h
@@ -32,5 +32,36 @@ namespace caches
 
 	};
 
-}
+	template<typename KeyT, typename T>
+	inline void cache_t<KeyT, T>::emplace(KeyT element)
+	{
+		T repetition = ++cache_[element];
 
+		// seek element in list
+		typename std::list<std::pair<KeyT, T>>::iterator iter_elem = std::find_if(objlist_.begin(), objlist_.end(), [&element](const std::pair<KeyT, T>& temp_p) { return element == temp_p.first; });
+
+		// seek repetition of an element 
+		typename std::list<std::pair<KeyT, T>>::iterator iter_num = std::find_if(objlist_.begin(), objlist_.end(), [&repetition](const std::pair<KeyT, T>& temp_p) { return repetition >= temp_p.second; });
+
+		if (iter_elem == objlist_.end())
+		{
+			if (iter_num != objlist_.end())
+			{
+				objlist_.emplace(iter_num, std::pair<KeyT, T>(element, cache_[element]));
+				objlist_.pop_back();
+			}
+		}
+		else // have found element
+		{
+			++iter_elem->second;
+			++hits_;
+
+			if (iter_num != objlist_.end())
+			{
+				objlist_.emplace(iter_num, std::pair<KeyT, T>(element, cache_[element]));
+				objlist_.erase(iter_elem);
+			}
+		}
+	}
+
+}
