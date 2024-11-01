2edbc49525a8830f04d953b26b8c0661f0f87d65
onelig ⠀ <onelig.dark@gmail.com>
2024-11-01 18:41:30 +0200

    realize main

--- HWC - cache(LFU)/main.cpp
+++ HWC - cache(LFU)/main.cpp
@@ -3,5 +3,18 @@
 
 int main()
 {
+	size_t s_cache, n_elements;
+	std::cin >> s_cache >> n_elements;
+	caches::cache_t<int> cache_obj(n_elements);
+
+	for (size_t i = 0; i < s_cache; ++i)
+	{
+		int value;
+		std::cin >> value;
+		cache_obj.emplace(value);
+	}
+
+	std::cout << cache_obj.GetHits();
+
 	return 0;
 }
